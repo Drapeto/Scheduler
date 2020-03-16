95b67352895cea65ecfd7a2ca0a0a2ddeb5055c6
Drapeto <drapetorres@gmail.com>
2020-03-16 12:46:13 -0700

    Scheduler

---
+++ Develop/script.js
@@ -0,0 +1,67 @@
+// Variable to display current date
+var today = moment().format('dddd, MMMM Do YYYY, h:mm a');
+console.log(today);
+// Logs time to Jumbotron area
+$("#currentDay").text(today);
+// for loop to create div elements on html page for timeblocks
+for (var i = 8; i < 18; i++) {
+    var pElement = $("<p></p>").text(i + ":00am ");
+    // if statement will switch from military to standard time and adds the class="hour" to the <p>.
+        // Added else if statement to be able to get 12:00pm to appear because 12:00am was ONLY appearing
+    if (i > 12) {
+        pElement.text(i - 12 + ":00pm ");
+    }
+    else if (i > 11) {
+        pElement.text(i - 0 + ":00pm ");
+    }
+    pElement.addClass("hour col-md-2")
+    // Local Variables to create the text areas to input data
+    var textField = $("<textarea></textarea>");
+    // add ID and input elements within textarea and what time it is on
+    textField.attr("id", "input"+i);
+    textField.addClass("description col-md-8");
+    // Logging to make sure textarea is being implemented
+    console.log(textField);
+    // Variable to create a button to save input data
+    var saveBtn = $("<button></button>").text("Save");
+    saveBtn.attr("name", i);
+    saveBtn.addClass("saveBtn col-md-2");
+    console.log(saveBtn);
+    //Variable to create div element on html for timeblocks
+    var divEl = $("<div></div>").append(pElement, textField, saveBtn);
+    divEl.addClass("time-block row");
+    console.log(divEl);
+    // if and else if statements to assign classes to past, present, future
+    // classes that are assigned will then be color coded reflecting past, present, future
+    if ( i > moment().hours()){
+        textField.addClass("future");
+    } else if ( i < moment().hours()) {
+        textField.addClass("past");
+    } else if ( i == moment().hours()) {
+        textField.addClass("present")
+    }
+    // Logging exact hour
+    console.log(moment().hours());
+    // To append all Divs "time-blocks to HTML"
+    $(".container").append(divEl);
+}
+// Click Event for saveButton
+$(document).on("click", ".saveBtn", function(){
+    // Variable for input data in textarea
+    var input = $("#input" + $(this).attr("name")).val();
+    // stores what input data gets received
+    localStorage.setItem($(this).attr("name"), input);
+});
+// Any data that gets input in the text areas will get stored even thru refresh
+// unless otherwise deleted and saved
+$("#input8").val(localStorage.getItem("8"));
+$("#input9").val(localStorage.getItem("9"));
+$("#input10").val(localStorage.getItem("10"));
+$("#input11").val(localStorage.getItem("11"));
+$("#input12").val(localStorage.getItem("12"));
+$("#input13").val(localStorage.getItem("13"));
+$("#input14").val(localStorage.getItem("14"));
+$("#input15").val(localStorage.getItem("15"));
+$("#input16").val(localStorage.getItem("16"));
+$("#input17").val(localStorage.getItem("17"));
+$("#input18").val(localStorage.getItem("18"));
